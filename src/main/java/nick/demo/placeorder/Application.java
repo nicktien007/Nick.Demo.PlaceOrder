ec602c9d3f050825324b9596bdd21821919eb5d5
nick <trojanhorse0077@gmail.com>
2021-10-05 01:38:24 +0800

    Initial commit

---
+++ src/main/java/nick/demo/placeorder/Application.java
@@ -0,0 +1,13 @@
+package nick.demo.placeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/nick/demo/placeorder/testMain.java
@@ -0,0 +1,12 @@
+package nick.demo.placeorder;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class testMain {
+    @Test
+    void testLog() {
+        log.info("aa");
+    }
+}
