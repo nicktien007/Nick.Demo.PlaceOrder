ab30732246ae2057fd5f921dacd171bed22e69eb
nick <trojanhorse0077@gmail.com>
2021-10-06 19:58:53 +0800

    add mainClass

---
+++ src/main/java/nick/demo/placeorder/MainClass.java
@@ -0,0 +1,11 @@
+package nick.demo.placeorder;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MainClass {
+    public static void main(String[] args) {
+
+        log.info("MainClass");
+    }
+}
